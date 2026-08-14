3150a4aae21f3d7cfeb2baef4e8f97498ed83414
ultra2106 <ta75005r@gmail.com>
2026-08-14 09:59:41 +0900

    Update api.js

--- src/js/api.js
+++ src/js/api.js
@@ -1,5 +1,5 @@
 // 🔧STEP3でコピーしたURLに書き換えてください
-const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbx0R1xqIlYl9_lGQ4Drcs3Bj3Qdn_AIBgctJa_9zpmqf2P5a6QijxniH9WCn_UKLV8s/exec';
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbwYt5_Xf5Wh7aaJ3fpSfj0FgG1h57sHHHYEs5BRjXzgj_rBvSf3oKA2pwOZbGNFhTXc/exec';
 
 // 🔧GASのスクリプトプロパティに設定したADMIN_SECRETと同じ値にしてください
 const ADMIN_SECRET = 'mahjong2026';
