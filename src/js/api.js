085af333c812243be5c1632144bfe0f99b00f484
ultra2106 <ta75005r@gmail.com>
2026-08-17 16:01:58 +0900

    Update api.js

--- src/js/api.js
+++ src/js/api.js
@@ -1,8 +1,9 @@
 const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbwYt5_Xf5Wh7aaJ3fpSfj0FgG1h57sHHHYEs5BRjXzgj_rBvSf3oKA2pwOZbGNFhTXc/exec';
 const ADMIN_SECRET = 'mahjong2026';
 
-async function apiGet(action) {
-  const res = await fetch(`${GAS_API_URL}?action=${action}`);
+async function apiGet(action, params) {
+  const query = params ? '&' + new URLSearchParams(params).toString() : '';
+  const res = await fetch(`${GAS_API_URL}?action=${action}${query}`);
   return res.json();
 }
 
@@ -14,42 +15,17 @@ async function apiPost(action, data) {
   return res.json();
 }
 
-async function getStandings() {
-  return apiGet('getStandings');
-}
+async function getStandings() { return apiGet('getStandings'); }
+async function getTeams() { return apiGet('getTeams'); }
+async function getPlayers() { return apiGet('getPlayers'); }
+async function getGames() { return apiGet('getGames'); }
+async function getGameDetail(id) { return apiGet('getGameDetail', { id }); }
 
-async function getTeams() {
-  return apiGet('getTeams');
-}
+async function submitGame(gameData) { return apiPost('addGame', gameData); }
+async function updateGame(gameData) { return apiPost('updateGame', gameData); }
+async function deleteGame(id) { return apiPost('deleteGame', { id }); }
 
-async function getPlayers() {
-  return apiGet('getPlayers');
-}
-
-async function getGames() {
-  return apiGet('getGames');
-}
-
-async function submitGame(gameData) {
-  return apiPost('addGame', gameData);
-}
-
-async function submitTeam(teamData) {
-  return apiPost('addTeam', teamData);
-}
-
-async function submitPlayer(playerData) {
-  return apiPost('addPlayer', playerData);
-}
-
-async function updateTeam(teamData) {
-  return apiPost('updateTeam', teamData);
-}
-
-async function deleteTeam(id) {
-  return apiPost('deleteTeam', { id });
-}
-
-async function deleteGame(id) {
-  return apiPost('deleteGame', { id });
-}
+async function submitTeam(teamData) { return apiPost('addTeam', teamData); }
+async function submitPlayer(playerData) { return apiPost('addPlayer', playerData); }
+async function updateTeam(teamData) { return apiPost('updateTeam', teamData); }
+async function deleteTeam(id) { return apiPost('deleteTeam', { id }); }
